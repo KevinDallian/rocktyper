da99809758a800e7f1532c61d1318623f43f5cf4
Kevin Dallian <kevindallian@gmail.com>
2023-05-23 10:11:50 +0700

    modified removeRocks logic

--- TyperGame/Scenes/GameScene.swift
+++ TyperGame/Scenes/GameScene.swift
@@ -14,32 +14,11 @@ class GameScene : SKScene{
     var wordManager = WordManager.shared
     var totalRocks = 0
     var currentRockIndex = 0
+    var currentTopRow = 0
     
     override func didMove(to view: SKView){
         super.didMove(to: view)
         self.initializeBackground()
-    }
-    
-    func initializeBackground(){
-        let background = SKSpriteNode(imageNamed: "background2")
-        background.position = CGPoint(x: frame.midX, y: frame.midY)
-        background.zPosition = -1
-        addChild(background)
-        
-        for row in 0..<3 {
-            for column in 0..<7 {
-                let rock = RockNode(text: wordManager.words[totalRocks])
-                rock.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-                rock.position = CGPoint(x: rock.size.height + rock.size.width * CGFloat(column), y: frame.maxY - 20 - CGFloat(row) * rock.size.height - rock.size.height)
-                rocks.append(rock)
-                addChild(rock)
-                totalRocks += 1
-            }
-        }
-        
-        textField = SKLabelNode(text: "")
-        textField?.position = CGPoint(x: frame.midX, y: frame.midY - 200)
-        addChild(textField!)
     }
     
     override func mouseUp(with event: NSEvent){
@@ -56,6 +35,9 @@ class GameScene : SKScene{
             if characterString == " " {
                 if checkWordIsTrue(word: textField!.text!, index: currentRockIndex){
                     nextRock()
+                    if currentRockIndex != 7 && currentRockIndex % 7 == 0 {
+                        generateRocks(row: 4)
+                    }
                 }
                 textField!.text! = ""
             } else if keyCode == 51 {
@@ -70,25 +52,82 @@ class GameScene : SKScene{
         }
     }
     
+    func initializeBackground(){
+        let background = SKSpriteNode(imageNamed: "background2")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.zPosition = -1
+        addChild(background)
+        
+        for row in 0..<3 {
+            for column in 0..<7 {
+                let rock = RockNode(text: wordManager.words[totalRocks])
+                rock.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+                rock.position = CGPoint(x: rock.size.height + rock.size.width * CGFloat(column), y: frame.maxY - 50 - CGFloat(row) * rock.size.height - rock.size.height)
+                rocks.append(rock)
+                addChild(rock)
+                totalRocks += 1
+            }
+        }
+        
+        textField = SKLabelNode(text: "")
+        textField?.position = CGPoint(x: frame.midX, y: frame.midY - 200)
+        addChild(textField!)
+    }
+   
+    
     func nextRock() {
         rocks[currentRockIndex].textNode.fontColor = .green
         currentRockIndex += 1
     }
     
-//    func moveRocksToLeft() {
-//        let initialXPosition: CGFloat = 100
-//        let rockSpacing: CGFloat = 10
-//        var index = 0
-//        for rock in rocks {
-//            let newXPosition = initialXPosition + CGFloat(index) * (rock.size.width + rockSpacing)
-//            let moveAction = SKAction.moveTo(x: newXPosition, duration: 0.2)
-//            rock.run(moveAction)
-//            index += 1
-//        }
-//    }
+    func generateRocks(row: Int) {
+        for column in 0..<7 {
+            let rock = RockNode(text: wordManager.words[totalRocks])
+            rock.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            rock.position = CGPoint(x: rock.size.height + rock.size.width * CGFloat(column), y: frame.maxY - 50 - CGFloat(row) * rock.size.height)
+            rock.setScale(0)
+            rocks.append(rock)
+            addChild(rock)
+            totalRocks += 1
+            let scaleAction = SKAction.scale(to: 1.0, duration: 0.2)
+            let moveRocksAction = SKAction.run(moveRocksToTop)
+            let removeTopRocksAction = SKAction.run(removeTopRocks)
+            let sequenceAction = SKAction.sequence([scaleAction, moveRocksAction, removeTopRocksAction])
+            rock.run(sequenceAction)
+        }
+        let delay = SKAction.wait(forDuration: 0.5)
+        self.run(delay){
+            self.currentTopRow += 1
+        }
+        
+    }
+    
+    func moveRocksToTop() {
+        let screenHeight = frame.height
+        var row = 0
+        var index = 0
+        for rockNode in rocks {
+            let moveAction = SKAction.move(to: CGPoint(x: rockNode.position.x, y: rockNode.position.y + 100), duration: 0.2)
+            rockNode.run(moveAction)
+            index += 1
+            if index % 7 == 0{
+                row += 1
+            }
+        }
+    }
+    
+    func removeTopRocks(){
+        let dissapear = SKAction.scale(to: 0, duration: 0.2)
+        let rotate = SKAction.rotate(toAngle: -2*Double.pi, duration: 0.2)
+        let dissapearAndRotate = SKAction.group([dissapear, rotate])
+        let topRow = (currentTopRow) * 7
+        print(topRow)
+        for index in 0..<7 {
+            rocks[index + topRow].run(SKAction.sequence([dissapearAndRotate, SKAction.removeFromParent()]))
+        }
+    }
     
     func checkWordIsTrue(word: String, index : Int) -> Bool{
         word == wordManager.words[index]
     }
-    
 }
